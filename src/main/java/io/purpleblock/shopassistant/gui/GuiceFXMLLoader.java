ce6c9cefd2dc34db796bc7e159644c2e989bd508
Marc Allen <allenmg@users.noreply.github.com>
2016-01-23 00:13:23 -0600

    handle relative path fxml includes.

--- src/main/java/io/purpleblock/shopassistant/gui/GuiceFXMLLoader.java
+++ src/main/java/io/purpleblock/shopassistant/gui/GuiceFXMLLoader.java
@@ -22,6 +22,7 @@ public class GuiceFXMLLoader {
 	public static final Injector injector = Guice.createInjector(new ApplicationModule());
 	public static <T> T load(URL location) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(location);
 		loader.setControllerFactory(new Callback<Class<?>, Object>() {
 			@Override
 			public Object call(Class<?> clazz) {
@@ -33,6 +34,7 @@ public class GuiceFXMLLoader {
 	
 	public <T> T load() throws IOException {
 		loader = new FXMLLoader();
+		loader.setLocation(location);
 		loader.setControllerFactory(new Callback<Class<?>, Object>() {
 			@Override
 			public Object call(Class<?> clazz) {
